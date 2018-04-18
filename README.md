84fe49e720f83032a72d41ef2e3e554ec846f99a
gobeltri <gobeltri@gmail.com>
2018-04-18 08:32:12 +0200

    Datascience libraries + Create a Table from Crowdlending tab

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-# Investion
+# Gonance
 
-### Development
+### Development (OS X)
 
-```python
+```shell
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install python
+ln -s /usr/local/bin/python3 /usr/local/bin/python
+ln -s /usr/local/bin/pip3 /usr/local/bin/pip
+pip install --user pipenv
 git clone https://github.com/gobeltri/gonance.git
 cd gonance
-pipenv install
+pipenv install   ## Patience is king
 pipenv shell
 ```
--- gonance.py
+++ gonance.py
@@ -2,6 +2,9 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from config import GonanceConfig
 
+from datascience.util import make_array
+from datascience import Table, CurrencyFormatter, PercentFormatter
+
 scope = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 credentials = ServiceAccountCredentials.from_json_keyfile_name(GonanceConfig.OAUTH2_SERVICE_KEY, scope)
 gspread_client = gspread.authorize(credentials)
@@ -9,7 +12,37 @@ gspread_client = gspread.authorize(credentials)
 # Example
 investion_sheet = gspread_client.open_by_key(GonanceConfig.INVESTION_SHEET_KEY)
 investion_historical = investion_sheet.worksheet('Historical')
-cell_list = investion_historical.range('B3:B12')
-for cell in cell_list:
-    print(cell.value)
+crowdlending_gsheet = []
+crowdlending_gsheet.append(investion_historical.range('A3:A12'))
+crowdlending_gsheet.append(investion_historical.range('B3:B12'))
+crowdlending_gsheet.append(investion_historical.range('C3:C12'))
+crowdlending_gsheet.append(investion_historical.range('D3:D12'))
+crowdlending_gsheet.append(investion_historical.range('E3:E12'))
+crowdlending_gsheet.append(investion_historical.range('F3:F12'))
+crowdlending_gsheet.append(investion_historical.range('G3:G12'))
 
+matrix = []
+for column in crowdlending_gsheet:
+	matrix.append([])
+	for cell in column:
+		if '%' in cell.value:
+			matrix[len(matrix)-1].append(float(cell.value.replace('%', '').replace(',', '')) / 100)
+		elif '€' in cell.value:
+			matrix[len(matrix)-1].append(float(cell.value.replace('€', '').replace(',', '')))
+		else:
+			matrix[len(matrix)-1].append(cell.value)
+
+crowdlending = Table().with_column(
+	'Period', matrix[0],
+	'Investment', matrix[1],
+	'Profit', matrix[2],
+	'Estimated Profit', matrix[3],
+	'Profit %', matrix[4],
+	'Invested', matrix[5],
+	'Bank', matrix[6]
+	)
+
+crowdlending.set_format([1,2,3,5,6], CurrencyFormatter(symbol='€'))
+crowdlending.set_format(4, PercentFormatter)
+print('Crowdlending')
+print(crowdlending)
